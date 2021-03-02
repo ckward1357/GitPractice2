48f9d4c3f11ed4ff873313990da76332e7404a67
Caeden <Ward>
2021-03-02 11:14:49 -0700

    added film posters

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -1,17 +1,23 @@
 import {films} from '../data/films.js'
 
 
-
 let filmList = document.querySelector('#filmList')
 
 
 for (let i = 0; i < films.length; i++) {
 
-    //let lastNum = getLastNumber(films[i].url)
+
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
-    let filmItem = document.createElement('li')
-    filmItems.textContent = film[i].title
-    filmList.appendChild(filmItem)
+    let posterFig = document.createElement('figure')
+    letfigImg = document.createElement (`img`)
+    figImg.src = 'https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg'
+    let figCaption = document.createElement (`figcaption`)
+
+    figCaption.textContent = founFilm.title
+    posterFig.appendChild(figImg)
+    posterFig.appendChild(figCaption)
+
+    filmList.appendChild(posterFig)
 }
 
 function getLastNumber (url) {

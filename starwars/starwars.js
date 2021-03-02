a220fb2f3a7ebf6301be728ad72a0797edb1d663
Caeden <Ward>
2021-03-02 11:28:43 -0700

    fixed all of the errors

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -1,4 +1,4 @@
-import {films} from '../data/films.js'
+import {films} from '../starwars/Data/films.js'
 
 
 let filmList = document.querySelector('#filmList')
@@ -9,11 +9,11 @@ for (let i = 0; i < films.length; i++) {
 
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
     let posterFig = document.createElement('figure')
-    letfigImg = document.createElement (`img`)
-    figImg.src = 'https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg'
-    let figCaption = document.createElement (`figcaption`)
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    let figCaption = document.createElement(`figcaption`)
 
-    figCaption.textContent = founFilm.title
+    figCaption.textContent = foundFilm.title
     posterFig.appendChild(figImg)
     posterFig.appendChild(figCaption)
 

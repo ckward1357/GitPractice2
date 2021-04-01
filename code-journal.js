5a8f486e38b101fa9b89d568a1e15edd52e92929
Caeden Ward <ckward1357@gmail.com>
2021-04-01 10:30:56 -0600

    reinstalled git, styled congress, started pokemon

--- code-journal.js
+++ code-journal.js
@@ -1,90 +1,236 @@
-/*Variables - containers that store values */  
+/* Variables - containers that store values */
 
-var name; //a declared variable, but not initialized (has no value) and its in the global scope (if anyone declares a value with the same name it undermines this value) 
-let foo; // decalred variable that can be changed
-const bar; // declared variable that cannot be changed. short of for "constant"
-const ANSWER = 42; // all caps indicates a constant. more of an etiquitte thing. const is declared and initialized with the value of 42
-//semicolons should be added to the end of each command but the javascript reader usually adds them if you don't
+var name // a declared variable, but not initialized and it's in the global scope (BAD)
 
+let foo // a declared variable that can be changed
 
-/*Fundamental Data Types*/
+const bar // a declared variable that cannot be changed - short for 'constant'
+
+const ANSWER = 42 // const is declared and initialized with the value 42 
 
 // String
 
-let string1 = 'Hello World!' 
+let string1 = 'Hello World!' // read '=' as 'is assigned the value of...'
 
-let string2 = 'Hello Utah!'
+let string2 = "Hello Utah!"
 
-let string3 = new String ('Hello World!')
+let string3 = new String('Hello World!')
 
-        //in javascript the = sign is known as the assignment operator. It reads as "is assigned the value of"
+// Number
 
-
-// Numbers
-let myNum = 8675309
+let myNum = 2083972347
 
 let myNum2 = 75.43
 
-'1' == 1 //toExponential. this statement is true because of type coercion and the loose equality check
-'1'=== 1 // false because this is strict equality checking
+'1' == 1 // this statement is true because of type coercion and loose equality checking
+'1' === 1 // false because this is strict equality checking
 
 // Boolean
-let myBool = true
 
+let myBool = true
 
 // Array
 
 let myArray = [] // this is an empty array
-//               0   1     2       3      4     javascript starts counting at zero
-let myArray2 = [42,'bob',myBool, ANSWER, true] //these are some valid items you can put in an array
+//              0     1      2        3      4
+let myArray2 = [42, 'Bob', myBool, ANSWER, true]
 
 let secondElement = myArray2[1]
 
-myArray2.push("Caeden") //added element to the end of the array
+myArray2.push('Thor') // added an element to the end of myArray2
 
-myArray2.unshift("Hello World") // added element to the beginning of the array
+myArray2.unshift('Hello World!')
 
-let myLongString = "as;dlfkjasd;fkjas;ldfja;sds;kl" //just an array of characters
+let mylongString = '32408usfjalieriweur938u425ksdjfowiur84uwrlwshdjfo8wuroiwejr4e' // just an array of characters
 
-myLongString.length //this counts the number of characters in a string
+mylongString.length
 
-//object
-
+// Object
 
 let minObject = {}
 
 const myCar = {
-        make: "Chevorlet",
-        color: "Red",
-        Year: "1965",
-        vin: "123123123123123"
+    make: "Chevrolet",
+    color: "Red",
+    year: "1965",
+    vin: "2390487sijweoru38lirehs"
+  };
+  
+  myCar.numDoors = 2;
+  
+  const anotherObject = {
+    wordz: ["foo", "bar", "baz"],
+    car: {
+      make: "McLaren",
+      model: "675LT"
+    },
+    awesomeness: true
+  };
+  
+  // Functions
+  
+  function myFunction() {
+    return "My greeting to you...";
+  }
+  
+  function sumTwoThings(one, two) {
+    // watch out for data type issues here!
+    return one + two; // if numbers, will add them.  If strings, will concatenate.
+  }
+  
+  // Arrow Functions
+
+  // a higher order function is a function that accepts another function as a parameter.
+// filter, map and reduce are the most popular, but forEach, every, find, and some are also HOFs
+
+const theFunction = () => { //multiple lines use curly braces and 'return' keyword
+  return 'I am awesome'
 }
-//Functions
-function sumTwoThings(one, two){
-return (one + two)
-}
 
-//Arrow Functions
- //a higher order functions is a function that accepts another function as a parameter.
- //filter,map and reduce are the  most poplular, but forEach, every, find, and some are also HOFs
- const theFunction = () => { // multiple lines use curly braces and. . .
-         return 'I am awesome'
- }
-
-
-//array helper method 'map'
-
-let filmURLs =[
-        "https://swapi.co/api/films/",
-        "https://swapi.co?api?films/5/",
-        "https://swapi.co?api?films/4/this one is longer... even longer",
-        "https://swapi.co?api?films/6?",
-        "https: "
-        "https://swapi.co?api?films/1"
+// Filter method example.  Filter returns an array of all elements that 'pass the test'
+const pilots = [
+  {
+    id: 2,
+    name: "Wedge Antilles",
+    faction: "Rebels"
+  },
+  {
+    id: 8,
+    name: "Ciena Ree",
+    faction: "Empire"
+  },
+  {
+    id: 40,
+    name: "Iden Versio",
+    faction: "Empire"
+  },
+  {
+    id: 66,
+    name: "Thane Kyrell",
+    faction: "Rebels"
+  }
 ];
 
+const rebels = pilots.filter((pilot) => pilot.faction === "Rebels");
+const empire = pilots.filter((pilot) => {
+  return pilot.faction === "Empire";
+});
+
+// Array helper method 'map'
+
+let filmURLs = [
+  "https://swapi.co/api/films/",
+  "https://swapi.co/api/films/5/",
+  "https://swapi.co/api/films/4/this one is longer... even longer",
+  "https://swapi.co/api/films/6/",
+  "https: ",
+  "https://swapi.co/api/films/1/"
+];
+
+const filmLengths = filmURLs.map((filmURL) => filmURL.length);
+
+const filmPlusMore = filmURLs.map((filmURL) => {
+  let filmObj = {
+    index: filmURL,
+    foo: 'Bar'
+  }
+  return filmObj
+})
 
 
+// Reduce example
+
+const swpilots = [
+        {
+          id: 10,
+          name: "Poe Dameron",
+          years: 14
+        },
+        {
+          id: 2,
+          name: "Temmin 'Snap' Wexley",
+          years: 30
+        },
+        {
+          id: 41,
+          name: "Tallissan Lintra",
+          years: 16
+        },
+        {
+          id: 99,
+          name: "Ello Asty",
+          years: 22
+        }
+      ];
+      
+      const totalYears = swpilots.reduce((acc, pilot) => acc + pilot.years, 0);
+      
+      const mostExpPilot = swpilots.reduce((oldest, pilot) => {
+        return (oldest.years || 0) > pilot.years ? oldest : pilot;
+      }, {});
+      
+      // Now we combine map, reduce, and filter
+      
+      var personnel = [
+        {
+          id: 5,
+          name: "Luke Skywalker",
+          pilotingScore: 98,
+          shootingScore: 56,
+          isForceUser: true
+        },
+        {
+          id: 82,
+          name: "Sabine Wren",
+          pilotingScore: 73,
+          shootingScore: 99,
+          isForceUser: false
+        },
+        {
+          id: 22,
+          name: "Zeb Orellios",
+          pilotingScore: 20,
+          shootingScore: 59,
+          isForceUser: false
+        },
+        {
+          id: 15,
+          name: "Ezra Bridger",
+          pilotingScore: 43,
+          shootingScore: 67,
+          isForceUser: true
+        },
+        {
+          id: 11,
+          name: "Caleb Dume",
+          pilotingScore: 71,
+          shootingScore: 85,
+          isForceUser: true
+        }
+      ];
+      
+      let jediPersonnel = personnel.filter((person) => person.isForceUser);
+      
+      let jediScores = jediPersonnel.map(
+        (jedi) => jedi.pilotingScore + jedi.shootingScore
+      );
+      
+      let totalJediScore = jediScores.reduce((acc, score) => acc + score, 0);
+      
+      const totalJediScoreChained = personnel
+        .filter((person) => person.isForceUser)
+        .map((jedi) => jedi.pilotingScore + jedi.shootingScore)
+        .reduce((acc, score) => acc + score, 0);
+      
+      // Ternary operator syntax: condition ? exprIfTrue : exprIfFalse
+      
+      const totalJediScoreReduce = personnel.reduce(
+        (acc, person) =>
+          person.isForceUser
+            ? acc + person.pilotingScore + person.shootingScore
+            : acc,
+        0
+      );
 
 
 // broken code for dialog box html
